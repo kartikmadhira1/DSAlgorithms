920defbccf456c119e45518f27dc3de140a3f395
Kartik Madhira <kartikmadhira1@gmail.com>
2018-10-30 01:47:03 -0400

    update

--- cpp/LinkListTemp.cpp
+++ cpp/LinkListTemp.cpp
@@ -11,10 +11,65 @@ using namespace std;
 //create a Node class first which has a value and a pointer pointing to the next Node
 template <typename T>
 class Node{
-public:
+public: //should all this be public????/
     T value;
     Node<T> *next;
-    Node(T val): value(val),next(NULL){}
 };
+
+//define a class of Linkedlist that runs on the simple nodes
+template <typename T>
+class LinkedList{
+public:
+    //will this be a pointer?
+    Node<T> *head;
+    Node<T> *tail;
+    void insert(Node<T> *node);
+    void printList();
+};
+
+template <typename T>
+void LinkedList<T>::printList() {
+Node<T> *current= new Node<T>;
+    while(head->next!=NULL){
+        cout<<current->value<<'\n';
+        current=current->next;
+    }
+   cout<<current->value<<'\n';
+}
+
+template <typename T>
+void LinkedList<T>::insert(Node<T> *node) {
+    // make a copy
+    //iterate till the current one points to a NULL
+    Node<T> *current = new Node<T>;
+    current=head;
+    while(current->next!=NULL){
+        current=current->next;
+    }
+    current->next=node;
+}
+
+int main(){
+    //make a new linkedlist
+    LinkedList<float> list;
+
+    Node<float> *first= new Node<float>;
+    Node<float> *second= new Node<float>;
+    Node<float> *third= new Node<float>;
+
+    first->value=12.34;
+    first->next=NULL;
+
+    second->value=2.34;
+    second->next=NULL;
+
+    third->value=31.3434;
+    third->next=NULL;
+
+    list.insert(first);
+    list.insert(second);
+    list.insert(third);
+    list.printList();
+}
 
 
